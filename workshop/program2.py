23cced2ea422911c9473ec91532a8ccd78fbc76c
Prabitha pallat <prabidas01@gmail.com>
2021-01-14 12:18:24 +0530

    updated program

--- workshop/program2.py
+++ workshop/program2.py
@@ -1,1 +1,12 @@
-print("haii")
+num=input("Enter the number:")
+i=1
+data=0
+pattern=""
+while(i<=int(num)):
+    res=num*i
+    pattern=pattern+"+"+res
+    data+= int(res)
+    i+=1
+
+pattern=pattern.lstrip("+")
+print(pattern)
