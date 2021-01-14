ec2f3284338bf2347a2f22cbb56445730505a866
Prabitha pallat <prabidas01@gmail.com>
2021-01-14 12:30:44 +0530

    moified programs

---
+++ language_foundamentals/swapping2.py
@@ -0,0 +1,8 @@
+num1=int(input("Enter your first number:"))
+num2=int(input("Enter your second number:"))
+temp=num1
+num1=num2
+num2=temp
+print("swapping numbers")
+print("num1=",num1)
+print("num2=",num2)
